edc99b7f3ab7d44b0a94a8ed64c418e806965f73
ruchesavenir-afk <ruches.avenir@gmail.com>
2026-01-20 13:21:54 +0100

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,1 +1,15 @@
-// Animations au scroll
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
